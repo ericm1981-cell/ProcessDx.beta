ed8e556e1c7355e81ba792d4c8905a03c56e7cfe
ericm1981-cell <ericm1981@gmail.com>
2026-04-24 06:21:11 -0400

    Delete Sw.js

--- Sw.js
+++
@@ -1,6 +0,0 @@
-// ProcessDx Service Worker
-const CACHE_NAME = 'processdx-v3';
-const ASSETS = ['/','/index.html','/manifest.json','/processdx-icon-192.png','/processdx-icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(cached=>{if(cached)return cached;return fetch(e.request).then(r=>{if(!r||r.status!==200)return r;const c=r.clone();caches.open(CACHE_NAME).then(ca=>ca.put(e.request,c));return r;}).catch(()=>caches.match('/index.html'));}));});
